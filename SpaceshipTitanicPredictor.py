14b78b219e4b1d8720580766db0a3c29dd689e0c
Yuluka Gigante <yulukeitor@gmail.com>
2023-05-16 01:22:45 -0500

    Stramlit prediction ready

--- SpaceshipTitanicPredictor.py
+++ SpaceshipTitanicPredictor.py
@@ -24,5 +24,32 @@ with st.expander("Data"):
 with st.expander("Pestaña 2"):
     st.write("Contenido de la Pestaña 2")
 
-with st.expander("Pestaña 3"):
-    st.write("Contenido de la Pestaña 3")
+with st.expander("Inferencia"):
+    st.text("Inputs para modelo")
+    
+    is_vip = st.checkbox("¿Es VIP el pasajero?")
+    vrdeck_expense = st.number_input("Gasto de VRDeck del pasajero", value=0)
+    foodcourt_expense = st.number_input("Gasto de FoodCourt del pasajero", value=0)
+    spa_expense = st.number_input("Gasto de Spa del pasajero", value=0)
+    age = st.number_input("Edad del pasajero", value=0)
+    roomservice_expense = st.number_input("Gasto de Room Service del pasajero", value=0)
+    shoppingmall_expense = st.number_input("Gasto de Shopping del pasajero", value=0)
+    
+    clicked = st.button("Predecir")
+    
+    if clicked:
+        st.text("Prediciendo...")
+        
+        result = model.predict(pd.DataFrame(
+        {
+            "VIP":[is_vip],
+            "VRDeck":[vrdeck_expense],
+            "FoodCourt":[foodcourt_expense],
+            "Spa":[spa_expense],
+            "Age":[age],
+            "RoomService":[roomservice_expense],
+            "ShoppingMall":[shoppingmall_expense]
+        }
+        ))
+        
+        st.text("La predicción dle modelo es: {}".format(result))
